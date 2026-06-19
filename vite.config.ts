898fd3fd2329a6f7cca272ea41892ddecf0f2462
thesvj <saij@iiitd.ac.in>
2026-06-19 17:51:43 +0530

    Fix Svelte hydration chunking

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,20 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('/node_modules/svelte/')) {
+						return 'svelte';
+					}
+
+					if (id.includes('/node_modules/@sveltejs/kit/')) {
+						return 'sveltekit';
+					}
+				}
+			}
+		}
+	}
 });
